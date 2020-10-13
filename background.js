f65a9234314ec6d7cbd23447b503a5c48ede5388
GijunMoon (기준 문) <gijunmoon@moonstudio.kro.kr>
2020-10-13 12:09:54 +0900

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 function checkForValidUrl(tabId, changeInfo, tab) {
-  // URL에 'g' 글자가 있을 시
   if (tab.url.indexOf('www.youtube.com/watch') > -1) {
     // 현재 페이지에 PageAction 적용
     chrome.pageAction.show(tabId);
